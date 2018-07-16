56e96c8137286e56c8dd565e25e8d09a333c5a82
simu <kecha.mohamed@gmail.com>
2018-07-16 22:37:00 +0800

    now this is working

--- src/main/java/com/example/demo/DemodemoApplication.java
+++ src/main/java/com/example/demo/DemodemoApplication.java
@@ -2,13 +2,18 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-//@EnableJpaRepositories(basePackageClasses = UserJpaRepository.class)
 @SpringBootApplication
-public class DemodemoApplication {
+public class DemodemoApplication extends SpringBootServletInitializer {
+    // SpringBootServletInitializer is needed if you want to deploy into a tomcat
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(DemodemoApplication.class);
+    }
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemodemoApplication.class, args);
-	}
-
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(DemodemoApplication.class, args);
+    }
 }
